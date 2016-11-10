272eb36e48eafda6c1b2f5369edf4d1e4b410e37
Radoslaw Baranan <th3erm4n@gmail.com>
2016-11-10 12:22:06 +0100

    added adding funcionality

--- src/git_example/Calc.java
+++ src/git_example/Calc.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Calc {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
+        System.out.println("Wprowadz a i b");
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println("Suma: " + (a + b));
     }
 }
