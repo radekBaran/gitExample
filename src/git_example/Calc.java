adcd9ea7bd7b2f42b7bd5a42ceb8d7366583c0d1
Radoslaw Baranan <th3erm4n@gmail.com>
2016-11-10 11:57:41 +0100

    merge

--- src/git_example/Calc.java
+++ src/git_example/Calc.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Scanner sc = new Scanner(System.in);
-=======
         Scanner input = new Scanner(System.in);
         
->>>>>>> calc_feature
     }
 }
