a013b89d66f3593d3fb0814fe7f31e9e217b2f6e
Radoslaw Baranan <th3erm4n@gmail.com>
2016-11-10 13:11:05 +0100

    added adding and substract methods and new funcionality

--- src/git_example/Calc.java
+++ src/git_example/Calc.java
@@ -1,17 +1,39 @@
 package git_example;
 
-import java.util.Scanner;
-
 public class Calc {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    private double a;
+    private double b;
 
-        System.out.println("Wprowadz a i b");
-        int a = input.nextInt();
-        int bbbbb = input.nextInt();
-        int x = input.nextInt();
-        int y = input.nextInt();
+    public Calc(double a, double b) {
+        setA(a);
+        setB(b);
+    }
 
-        System.out.println("Suma: " + (a + b));
+    public Calc(){
+
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double add(double a, double b) {
+        return getA() + getB();
+    }
+
+    public double sub(double a, double b) {
+        return getA() - getB();
     }
 }
