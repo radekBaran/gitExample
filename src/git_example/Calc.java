b20fecb373cac0a0e85affcabfd8a0743c754311
Radoslaw Baranan <th3erm4n@gmail.com>
2016-11-10 11:40:44 +0100

    added scanner to my class

--- src/git_example/Calc.java
+++ src/git_example/Calc.java
@@ -1,7 +1,10 @@
 package git_example;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
     }
 }
